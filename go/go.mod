f6a157f84a29e8da70885ec2e77a620e62f93df2
Thomas Andre <thomasandre84@gmail.com>
2020-07-04 23:16:32 +0200

    example for login logout in go added

--- go/go.mod
+++ go/go.mod
@@ -1,3 +1,8 @@
 module samplesftpclients/sftp_clients
 
 go 1.14
+
+require (
+	github.com/pkg/sftp v1.11.0 // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+)
--- go/sftp_clients.go
+++ go/sftp_clients.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/pkg/sftp"
+	"golang.org/x/crypto/ssh"
+)
 
 func main() {
-		fmt.Println("Hello, sftp Client.")
+	fmt.Println("Hello, sftp Client.")
+	addr := "host:22"
+	config := &ssh.ClientConfig{
+		User: "user",
+		Auth: []ssh.AuthMethod{
+			ssh.Password("password"),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+		//Ciphers: []string{"3des-cbc", "aes256-cbc", "aes192-cbc", "aes128-cbc"},
 	}
+	conn, err := ssh.Dial("tcp", addr, config)
+	if err != nil {
+		panic("Failed to dial: " + err.Error())
+	}
+	client, err := sftp.NewClient(conn)
+	if err != nil {
+		panic("Failed to create client: " + err.Error())
+	}
+	// Close connection
+	defer client.Close()
+	cwd, err := client.Getwd()
+	println("Current working directory:", cwd)
+}
